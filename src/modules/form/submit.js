b4191661a7b41677341e206f919315f8dc96103d
dev-FilipeBCS <filipebregaco@gmail.com>
2025-03-28 18:21:50 +0100

    Create object when submitted

--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -1,6 +1,7 @@
 import dayjs from "dayjs"
 
 const form = document.querySelector("form")
+const clientName = document.getElementById("client")
 const selectedDate = document.getElementById("date")
 
 // Current date for input
@@ -13,5 +14,37 @@ selectedDate.value = inputToday
 form.onsubmit = (event) => {
   event.preventDefault()
 
-  console.log("Enviado!")
+  try {
+    // Retrieve client name
+    const name = clientName.value.trim()
+
+    if(!name) {
+      return alert("Informe o nome do cliente!")
+    }
+
+    // Retrieve the selected schedule hour
+    const hourSelected = document.querySelector(".hour-selected")
+
+    if(!hourSelected) {
+      return alert("Selecione uma hora.")
+    }
+
+    // Retrieve the hour of selected schedule hour
+    const [hour] = hourSelected.innerText.split(":")
+
+    // insert hour to date
+    const when = dayjs(selectedDate.value).add(hour, "hour")
+
+    const id = new Date().getTime()
+
+    console.log({
+      id,
+      name,
+      when,
+    })
+
+  } catch (error) {
+    alert("Não foi possível realizar o agendamento")
+    console.log(error)
+  }
 }
